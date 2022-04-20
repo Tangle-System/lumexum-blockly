43daefcbc850b140ce74d4954738be138adb6842
Lukáš Kovář <lkov@post.cz>
2022-04-20 18:54:49 +0200

    dragNdrop anywhere, buttons are nolonger focused after click

--- control.js
+++ control.js
@@ -795,13 +795,61 @@ document.body.ondrop = function (e) {
 };
 
 function handleFileDrop(e) {
+  e.preventDefault();
+
   console.log("ondrop", e);
   console.log(e.dataTransfer.files);
-  parseInputXMLfile(e.dataTransfer.files[0]);
-  e.preventDefault();
+  if(e.dataTransfer.files[0].name.endsWith(".tgbl")) {
+    parseInputXMLfile(e.dataTransfer.files[0]);
+  } else if (e.dataTransfer.files[0].name.endsWith(".zip")) {
+    parseInputXMLfile(e.dataTransfer.files[0]);
+  } else {
+    const music_url = URL.createObjectURL(e.dataTransfer.files[0]);
+    // Code.music.setAttribute("src", music_url);
+    wavesurfer.load(music_url);
+  }
 }
 
-document.querySelector("#filename").ondrop = handleFileDrop;
-document.querySelector("#loadFile").ondrop = handleFileDrop;
-document.querySelector("#saveFile").ondrop = handleFileDrop;
+// document.querySelector("#filename").ondrop = handleFileDrop;
+// document.querySelector("#loadFile").ondrop = handleFileDrop;
+// document.querySelector("#saveFile").ondrop = handleFileDrop;
+document.querySelectorAll("body *").forEach(e => e.addEventListener('drop',handleFileDrop)); 
+
 //document.querySelector(".blocklySvg").ondrop = handleFileDrop; // This doesn't work
+
+function drag_start(event) {
+  var style = window.getComputedStyle(event.target, null);
+  event.dataTransfer.setData("text/plain",
+  (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
+} 
+
+function drop(event) {
+  var offset = event.dataTransfer.getData("text/plain").split(',');
+  var dm = document.getElementById('dragme');
+  dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
+  dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
+  event.preventDefault();
+  return false;
+}
+
+function drag_over(event) {
+  event.preventDefault();
+  return false;
+} 
+
+// var dm = document.getElementById('dragme');
+// dm.addEventListener('dragstart',drag_start,false);
+document.body.addEventListener('dragover',drag_over,false);
+document.body.addEventListener('drop',drop,false); 
+
+function preventFocus(e){
+  setTimeout(() => {
+    e.target.blur()
+    // document.querySelector('body').click()
+    // document.querySelector('body').focus()
+
+
+  },1)
+}
+
+document.querySelector('.tabmax').addEventListener('click',preventFocus);
