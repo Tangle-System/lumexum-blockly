41a78951ec45430e1343af60f2790b6a15282298
VikinnCz <viktor@soundville.cz>
2022-04-21 12:44:25 +0200

    Add: Live microphone data chart

--- control.js
+++ control.js
@@ -264,26 +264,26 @@ window.onload = function () {
     event_logs.appendChild(logmessageDOM);
     event_logs.scrollTop = -999999999;
 
-    if(event_logs.childElementCount > 100){
+    if (event_logs.childElementCount > 100) {
       event_logs.removeChild(event_logs.firstChild)
     }
-    
+
   }
 
   let microphoneRunning = false;
 
   function handleControlSound() {
-    if(microphoneRunning){
+    if (microphoneRunning) {
       microphoneRunning = false;
-      control_sound.textContent ="Sound OFF";
-    } else{
+      control_sound.textContent = "Sound OFF";
+    } else {
       microphoneRunning = true;
       control_sound.textContent = "Sound ON";
 
     }
 
     var webaudio_tooling_obj = function () {
-    
+
       // Uává velikost bloků ze kterých bude vypočítávána průměrná hlasitos.
       // Maximální velikost je 2048 vzorků.
       // Hodnota musí být vždy násobkem dvou.
@@ -295,58 +295,62 @@ window.onload = function () {
       console.log("audio is starting up ...");
 
       var microphone_stream = null,
-          gain_node = null,
-          script_processor_get_audio_samples = null;
+        gain_node = null,
+        script_processor_get_audio_samples = null;
 
+      var CHART = document.getElementById('chart');
 
       // Dotaz na povolení přístupu k mikrofonu
       if (!navigator.getUserMedia)
-              navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia || navigator.msGetUserMedia;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-      if (navigator.getUserMedia){
+      if (navigator.getUserMedia) {
 
-          navigator.getUserMedia({audio:true}, 
-            function(stream) {
-                start_microphone(stream);
-            },
-            function(e) {
-              alert('Error capturing audio.');
-            }
-          );
+        navigator.getUserMedia({ audio: true },
+          function (stream) {
+            start_microphone(stream);
+          },
+          function (e) {
+            alert('Error capturing audio.');
+          }
+        );
 
       } else { alert('getUserMedia not supported in this browser.'); }
 
 
       // Funkce pro zahajující naslouchání mikrofonu
-      function start_microphone(stream){
-      
+      function start_microphone(stream) {
+
         gain_node = audioContext.createGain();
-        gain_node.gain.setValueAtTime(1,audioContext.currentTime);
+        gain_node.gain.setValueAtTime(1, audioContext.currentTime);
         microphone_stream = audioContext.createMediaStreamSource(stream);
         script_processor_get_audio_samples = audioContext.createScriptProcessor(BUFF_SIZE, 1, 1);
 
-        gain_node.connect( script_processor_get_audio_samples );
+        gain_node.connect(script_processor_get_audio_samples);
         script_processor_get_audio_samples.connect(audioContext.destination);
         microphone_stream.connect(gain_node);
 
-        
+
         console.log("Sample rate of soundcard: " + audioContext.sampleRate);
         var fft = new FFT(BUFF_SIZE, audioContext.sampleRate);
 
+        Plotly.newPlot(CHART, [{
+          y: [0],
+          type: 'line'
+        }]);
 
-        // var bufferCount = 0;
-
+        let cnt;
 
         // Tato funkce se provede pokaždé když dojde k naplnění bufferu o velikosti 2048 vzorků.
         // Při vzorkovacím kmitočku 48 kHz se tedy zavolá jednou za cca 42 ms.
-        script_processor_get_audio_samples.onaudioprocess = function(e) {
-          
+        script_processor_get_audio_samples.onaudioprocess = function (e) {
+
           var samples = e.inputBuffer.getChannelData(0);
           var rms_loudness_spectrum = 0;
           fft.forward(samples); //Vyypočtení fft ze vzorků.
           var spectrum = fft.spectrum; // Získání spektra o délce bufeer/2 v našem případě 1024 harmonických.
-          
+
           //--- Výpočet frekvence ---//
           //
           //    ((BufferSize/2)* Fvz)/BufferSize = Fmax
@@ -355,39 +359,34 @@ window.onload = function () {
           //------------------------//
 
           // Zde se postupně sečte druhá mocnina všech 1024 vzorků.
-          spectrum.forEach(element =>{
-            rms_loudness_spectrum += Math.pow(element,2);
+          spectrum.forEach(element => {
+            rms_loudness_spectrum += Math.pow(element, 2);
           });
 
-          // for (let i = 30; i < 50; i++) {
-          //   rms_loudness_spectrum += Math.pow(spectrum[i],2);
-          // }
 
           // Odmocnina součtu druhých mocnin nám dá efektivní hodnotu signálu "RMS"
           rms_loudness_spectrum = Math.sqrt(rms_loudness_spectrum);
 
           // Mapování efektivní hodnoty signálu na rozmezí 0-255 pro vhodný přenos dat.
           // Zde je zejmána nutné dobře nastavit mapovací prahy. Spodní pro odstranění šumu okolí a horní nám udává výslednou dynamiku.
-          var out =  mapValue(rms_loudness_spectrum, 0.00001, 0.9, 0, 255)
-                  
-          // console.log("spectrum avarge loudnes: "+ out);
+          var out = mapValue(rms_loudness_spectrum, 0.00001, 0.9, 0, 255)
+
           handleControlSend(out);
-          if(!microphoneRunning){
+          Plotly.extendTraces(CHART, { y: [[out]] }, [0]);
+
+          if(cnt > 500) {
+            Plotly.relayout(CHART,{
+                 xaxis: {
+                           range: [cnt-500,cnt]
+                        }
+            });
+         }
+
+          if (!microphoneRunning) {
             microphone_stream.disconnect();
-            gain_node.disconnect(); 
+            gain_node.disconnect();
             script_processor_get_audio_samples.disconnect();
           }
-
-          // if (bufferCount >= 5){
-          //     bufferCount = 0;
-          //     avarage_loudness = avarage_loudness/(BUFF_SIZE*6);
-          //     avarge_loudness_spectrum = avarge_loudness_spectrum/(BUFF_SIZE*3);
-
-          //     console.log("sample avarge loudnes: "+ mapValue(avarage_loudness,0.0005,0.05,0,255)); // This values set tresholds for noise and dynamics of signal.
-          //     console.log("spectrum avarge loudnes: "+ mapValue(avarge_loudness_spectrum, 0.00001, 0.0001, 0, 255));
-          //   } else {
-          //     bufferCount++;
-          // }
         };
       }
 
@@ -395,31 +394,31 @@ window.onload = function () {
         if (in_max == in_min) {
           return out_min / 2 + out_max / 2;
         }
-    
+
         let minimum = Math.min(in_min, in_max);
         let maximum = Math.max(in_min, in_max);
-    
+
         if (x < minimum) {
           x = minimum;
         } else if (x > maximum) {
           x = maximum;
         }
-    
+
         let result = ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-    
+
         minimum = Math.min(out_min, out_max);
         maximum = Math.max(out_min, out_max);
-    
+
         if (result < minimum) {
           result = minimum;
         } else if (result > maximum) {
           result = maximum;
         }
-    
+
         return result;
       }
 
-  }();
+    }();
   }
 
   control_percentage_range.oninput = handlePercentageValueChange;
@@ -508,7 +507,7 @@ function setupOwnership() {
       if (e != null) {
         owner_identifier.value = "";
       }
-    } catch {}
+    } catch { }
 
     owner_signature.value = Code.device.getOwnerSignature();
 
@@ -518,7 +517,7 @@ function setupOwnership() {
   owner_key.onchange = e => {
     try {
       Code.device.setOwnerKey(owner_key.value);
-    } catch {}
+    } catch { }
 
     owner_key.value = Code.device.getOwnerKey();
 
